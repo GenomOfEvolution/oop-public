fdde20efb5bb4fb6ad91a8cc15264eac9f31e225
Oleg Nechaev <oleg_nechaev_2019@mail.ru>
2025-03-18 12:22:44 +0300

    started work with args

--- lw2/ExpandTemplate/ExpandTemplate/main.cpp
+++ lw2/ExpandTemplate/ExpandTemplate/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <optional>
 #include "ExpandTemplate.h"
 
-int main()
+struct Args
+{
+    std::map<std::string, std::string> params;
+    bool isStdIn = false;
+    std::string inputFileName;
+    std::string outputFileName;
+};
+
+void PrintHelp()
+{
+    std::cout << "Usage: \n" <<
+        "ExpandTemplate.exe <input-file> <output-file> [<param> <value> [<param> <value> ...]] \n" <<
+        "OR ExpandTemplate.exe\n" <<
+        " [<param> := <value> \\n ...]\n" <<
+        " <empty line>\n" <<
+        " <lines to replace templates>\n" <<
+        "OR ExpandTemplate.exe -h to show this message";
+}
+
+std::optional<Args> ParseArgs(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+       
+    }
+}
+
+int main(int argc, char* argv[])
 {
     std::string tpl = "-AABBCCCCCABC+";
     std::map<std::string, std::string> params = {
